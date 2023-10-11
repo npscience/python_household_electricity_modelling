a20a9c2503cb8be7321cac5de00a0dd0eabe9ee0
Naomi Penfold <9406530+npscience@users.noreply.github.com>
2023-10-11 16:16:27 +0100

    initiate cleaning script

---
+++ clean_data.py
@@ -0,0 +1,13 @@
+## based on original final project script: prepare_household_energy_data.R
+## to read in raw csv and output cleaned data "daily_energy_all_hholds.csv"
+
+# read in raw csv
+
+
+# clean column names (all to lowercase, snake case)
+
+# add columns for month, season, weekday type, quarter, season (so can filter for Summer and Winter 2013)
+
+# trim data for dates: 2011-12-01 to 2014-02-27
+
+# write to new csv
